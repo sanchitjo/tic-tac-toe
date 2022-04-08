a3cec4a3cc5675b82267cd874aee13086ccda8e5
Sanchit Joshi <sanchityj@gmail.com>
2022-04-08 16:50:54 +0530

    added a reset button and changed the winner message

--- script.js
+++ script.js
@@ -3,31 +3,37 @@ cell = Array.from(cells)
 console.log(cells)
 
 let currentPlayer = 'X'
-
+let gameActive = true
 let winningCombination = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ]
 
-function checkForWinner(){
-    winningCombination.forEach(function(combination) {
+document.getElementById("reset-btn").addEventListener("click", resetHandler)
+function resetHandler() {
+    location.reload()
+}
+
+
+function checkForWinner() {
+    winningCombination.forEach(function (combination) {
         let check = combination.every(index => cells[index].innerText == currentPlayer)
-        if(check){
-            window.alert(currentPlayer + ' has won!')
+        if (check) {
+           document.getElementById("container").innerHTML = currentPlayer + ' has won!'
         }
-        
+
     })
 }
 
-cells.forEach(function(cell){
-    cell.addEventListener('click', function(){
-        if(cell.innerText != '') return
+cells.forEach(function (cell) {
+    cell.addEventListener('click', function () {
+        if (cell.innerText != '') return
         cell.innerText = currentPlayer
         checkForWinner()
         currentPlayer = currentPlayer === 'X' ? 'O' : 'X'
